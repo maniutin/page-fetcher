f69d8d2d40a3b740b99105643f0442b5c288aa4f
Nick Maniutin <maniutin@gmail.com>
2021-02-11 01:03:28 +0000

    logs errors

--- fetcher.js
+++ fetcher.js
@@ -3,6 +3,10 @@ const args = process.argv.slice(2);
 
 const request = require("request");
 request(args[0], (error, response, body) => {
+  if (error) {
+    console.log(error);
+    process.exit();
+  }
   fs.writeFile(args[1], body, function (err) {
     if (err) return console.log(err);
     console.log(`Downloaded and saved ${body.length} bytes to ${args[1]}`);
