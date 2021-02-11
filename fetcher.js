bd83fbc6d5c0b4d544f934927bb9a5ec04ba64ca
Nick Maniutin <maniutin@gmail.com>
2021-02-11 00:46:36 +0000

    solves fetcher

---
+++ fetcher.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const args = process.argv.slice(2);
+
+const request = require("request");
+request(args[0], (error, response, body) => {
+  fs.writeFile(args[1], body, function (err) {
+    if (err) return console.log(err);
+    console.log(`Downloaded and saved ${body.length} bytes to ${args[1]}`);
+  });
+});
